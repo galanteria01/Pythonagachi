1258113bd486305d6e7b9893ae81e5cdb478ef26
shanu12e <shanuu12e@gmail.com>
2020-06-09 21:35:38 +0530

    Initialised game

---
+++ classes/game.py
@@ -0,0 +1,7 @@
+class pet():
+    def __init__(self,name,level):
+        self.name=name
+        self.level=level
+
+    def details(self):
+
---
+++ main.py
@@ -0,0 +1,6 @@
+from classes.game import pet
+print("Welcome to Pythonagachi Simulator Application")
+difficulty_level = int(input("Enter the difficulty level: "))
+name=input("Enter the name of your pet: ")
+
+player=pet(name,difficulty_level)
