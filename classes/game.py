7de510024fbf4ff40fd806231d0d5389aae69cdb
shanu12e <shanuu12e@gmail.com>
2020-06-09 21:53:38 +0530

    Made few here and there changes

--- classes/game.py
+++ classes/game.py
@@ -1,7 +1,22 @@
+import random
 class pet():
-    def __init__(self,name,level):
+    def __init__(self,name,level,hunger,boredom,tiredness,dirtiness):
         self.name=name
         self.level=level
+        self.hunger=hunger
+        self.boredom=boredom
+        self.tiredness=tiredness
+        self.dirtiness=dirtiness
+
 
     def details(self):
+        print("Creature Name:",self.name)
+        print("Hunger(0-10)",self.hunger)
+        print("Boredom(0-10)", self.boredom)
+        print("Tiredness(0-10)",self.tiredness)
+        print("Dirtiness(0-10)",self.dirtiness)
+
+
+
+
 
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from classes.game import pet
+
+#Initialising game
 print("Welcome to Pythonagachi Simulator Application")
 difficulty_level = int(input("Enter the difficulty level: "))
-name=input("Enter the name of your pet: ")
+name=input("Enter the name of your pet: ").capitalize()
 
-player=pet(name,difficulty_level)
+#Initialising player
+player=pet(name,difficulty_level,0,0,0,0)
+
+choice=["eat","play","sleep","take a bath","forage for food"]
+
+
+running=True
+i=1
+while running:
+   print("Round",str(i))
+   print("\n")
+   player.details()
+   running=False
