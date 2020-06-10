ec647afc4f5c6e4e1634242e8c96ce0ed42a06b4
shanu12e <shanuu12e@gmail.com>
2020-06-10 20:54:59 +0530

    Some more modifications

--- classes/game.py
+++ classes/game.py
@@ -1,12 +1,17 @@
 import random
 class pet():
-    def __init__(self,name,level,hunger,boredom,tiredness,dirtiness):
+    def __init__(self,name,level,hunger,boredom,tiredness,dirtiness,choice,food_forage,status):
         self.name=name
         self.level=level
         self.hunger=hunger
         self.boredom=boredom
         self.tiredness=tiredness
         self.dirtiness=dirtiness
+        self.choice=choice
+        self.food_forage=food_forage
+        self.status=status
+
+
 
 
     def details(self):
@@ -15,8 +20,38 @@ class pet():
         print("Boredom(0-10)", self.boredom)
         print("Tiredness(0-10)",self.tiredness)
         print("Dirtiness(0-10)",self.dirtiness)
+        print("\n")
+        print("Food stored",str(self.food_forage))
+        print("Status",self.status)
 
+    def get_choices(self,choice):
+        i=1
+        for cho in choice:
+            print("Enter",str(i),"for",cho)
+            i+=1
 
+    def eating(self):
+        self.hunger=0
+        self.food_forage-=1
 
+    def playing(self):
+        self.boredom=0
+        k=random.randrange(1,self.level)
+        self.tiredness+=k
+        self.dirtiness+=k
+
+    def sleeping(self):
+        self.tiredness=0
+        k=random.randrange(0,self.level)
+        self.dirtiness+=k
+        self.boredom+=k
+
+    def bathing(self):
+        if self.dirtiness<10:
+            self.dirtiness=0
+        k=random.randrange(0,self.level)
+        self.tiredness+=k
+        if self.tiredness>10:
+            self.status="sleep"
 
 
--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from classes.game import pet
 print("Welcome to Pythonagachi Simulator Application")
 difficulty_level = int(input("Enter the difficulty level: "))
 name=input("Enter the name of your pet: ").capitalize()
+choices=["eat","play","sleep","take a bath","forage for food"]
 
 #Initialising player
-player=pet(name,difficulty_level,0,0,0,0)
+player=pet(name,difficulty_level,0,0,0,0,choices,2,"awake")
 
-choice=["eat","play","sleep","take a bath","forage for food"]
 
 
 running=True
@@ -17,4 +17,14 @@ while running:
    print("Round",str(i))
    print("\n")
    player.details()
+   player.get_choices(choices)
+   player_choice=int(input("Enter the choice of player: "))
+   if player_choice==1:
+      player.eating()
+   elif player_choice==2:
+      player.playing()
+   elif player_choice==3:
+
+
+
    running=False
